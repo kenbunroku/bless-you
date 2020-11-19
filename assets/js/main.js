d20d8c1aa1f8700dc2b929998d1f0dd68d9c0ad6
Shinichiro Kuwahara <kenbunroku524@gmail.com>
2020-11-19 07:04:37 -0600

    Add actions

--- assets/js/main.js
+++ assets/js/main.js
@@ -19,13 +19,25 @@ const config = {
 let player;
 let hearts;
 let sneezeBar;
+let cursors;
 
 const game = new Phaser.Game(config);
 
 function preload() {
   this.load.image('building', './assets/img/background.png');
   this.load.image('heart', './assets/img/heart.png');
-  this.load.image('sneezeBar', './assets/img/sneeze-bar.gif');
+  this.load.spritesheet('sneezeBar', './assets/img/sneeze-bar.png', {
+    frameWidth: 150,
+    frameHeight: 50,
+  });
+  this.load.spritesheet('sickHero', './assets/img/sick-hero.png', {
+    frameWidth: 32,
+    frameHeight: 32,
+  });
+  this.load.spritesheet('villain', './assets/img/villain.png', {
+    frameWidth: 32,
+    frameHeight: 32,
+  });
 }
 
 function create() {
@@ -38,7 +50,38 @@ function create() {
   });
 
   sneezeBar = this.physics.add.staticGroup();
-  sneezeBar.create(240, 30, 'sneezeBar')
+  sneezeBar.create(250, 30, 'sneezeBar');
+
+  player = this.physics.add.sprite(400, 550, 'sickHero');
+
+  player.setBounce(0.2);
+  player.setCollideWorldBounds(true);
+
+  this.anims.create({
+    key: 'turn',
+    frames: [{ key: 'sickHero', frame: 1 }],
+    frameRate: 20,
+  });
+
+  this.anims.create({
+    key: 'right',
+    frames: this.anims.generateFrameNumbers('sickHero', { start: 7, end: 8 }),
+    frameRate: 5,
+    repeat: -1,
+  });
+
+  cursors = this.input.keyboard.createCursorKeys();
 }
 
-function update() {}
+function update() {
+  if (cursors.right.isDown) {
+    player.setVelocityX(100);
+
+    player.anims.play('right', true);
+  } else
+  {
+    player.setVelocityX(0);
+
+    player.anims.play('turn')
+  }
+}
